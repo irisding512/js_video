6003fc0db82edf602293f974d0dbf7f4bda8ae83
irisding512 <113000709+irisding512@users.noreply.github.com>
2023-11-06 15:40:51 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -3,9 +3,20 @@ var video;
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
 
+	//Page load - initialize the video element, turn off autoplay & looping
+	video = document.querySelector("#player1")
+	video.autoplay = false;
+	video.pause();
+	video.loop = false;
+	video.volume = document.querySelector("#slider").value / 100;
+
+	document.querySelector("#play").addEventListener("click", function() {
+		console.log("Play Video");
+		video.play();
+	});
 });
 
-document.querySelector("#play").addEventListener("click", function() {
-	console.log("Play Video");
-});
+
+
+
 
